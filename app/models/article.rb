061657e74229c81ef37cc537ce3a68bbb4e3f804
App Academy Student <student@appacademy.io>
2014-02-15 14:02:10 -0800

    Need to complete taggings

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ActiveRecord::Base
+  has_many :comments,
+  :class_name => 'Comment',
+  :foreign_key => :article_id,
+  :primary_key => :id
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '97a6a44c5b976f9d6d5eb9b4e4e9da63bae10bd9429678240bd792e1014fbd8d848d2566eecf97b5ce38f8cbe28b2763c8d56f01e21a484b49a07b2bf6e7f1d0'
