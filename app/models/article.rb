dba1e56163c23047fc74793cb0bcacb5fd34c0e6
App Academy Student <student@appacademy.io>
2014-02-15 15:48:05 -0800

    final commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,15 @@ class Article < ActiveRecord::Base
   :class_name => 'Comment',
   :foreign_key => :article_id,
   :primary_key => :id
+
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
+
 end
